b702f32adaae82de719d87fde4bfdaf0a565cf13
ravikumargh <ravikumargh@gmail.com>
2023-11-16 16:41:52 +0530

    React Font Awesome set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,11 +6,14 @@ import Home from './components/containers/Home';
 import Login from './components/containers/Login';
 import Profile from './components/containers/Profile';
 import NoPage from './components/containers/NoPage';
+ 
+
+// library.add(fas, farFaCoffee)
 
 function App() {
   return (
     <div className="App">
-       
+        
       <BrowserRouter>
       
         <Routes>
--- src/components/layouts/Navigation.js
+++ src/components/layouts/Navigation.js
@@ -2,18 +2,21 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="/">Matrix
-      <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"></img>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>
+            <FontAwesomeIcon icon={faHome} />
+            </Nav.Link>
           </LinkContainer>
           <LinkContainer to="/login">
             <Nav.Link>Login</Nav.Link>
