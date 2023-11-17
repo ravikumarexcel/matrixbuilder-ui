ada3c09a6a9edea317bb179377cb0f72025ba5ce
ravikumargh <ravikumargh@gmail.com>
2023-11-17 13:10:21 +0530

    login - handle login function definition

--- src/components/containers/Login.js
+++ src/components/containers/Login.js
@@ -1,10 +1,25 @@
 // Login.js
-import React from 'react';
+import React, { useState } from 'react';
 // import { Button } from 'react-bootstrap';
 import './Login.css';
 import logo from './../../logo.svg';
 
 const Login = () => {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+const handleUsernameChange =(e) =>{
+  setUsername(e.target.value);
+}
+
+const handlePassword = (e) =>{
+  setPassword(e.target.value);
+}
+
+const handleLogin = () =>{
+  console.log('Login ', {username, password});
+}
+
   return (
     <div>
       {/* <h1>Login Page</h1>
@@ -16,28 +31,28 @@ const Login = () => {
   </Button> */}
 
 
-      <main class="form-signin w-100 m-auto">
+      <main className="form-signin w-100 m-auto">
         <form>
-          <img class="mb-4" src={logo} alt="" width="72" height="57" />
-          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+          <img className="mb-4" src={logo} alt="" width="72" height="57" />
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-          <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label for="floatingInput">Email address</label>
+          <div className="form-floating">
+            <input type="email" value={username} onChange={handleUsernameChange} className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Email address</label>
           </div>
-          <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-            <label for="floatingPassword">Password</label>
+          <div className="form-floating">
+            <input type="password" value={password} onChange={handlePassword} className="form-control" id="floatingPassword" placeholder="Password" />
+            <label htmlFor="floatingPassword">Password</label>
           </div>
 
-          <div class="form-check text-start my-3">
-            <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
-            <label class="form-check-label" for="flexCheckDefault">
+          <div className="form-check text-start my-3">
+            <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
               Remember me
             </label>
           </div>
-          <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-          <p class="mt-5 mb-3 text-body-secondary">© 2023–2028</p>
+          <button className="btn btn-primary w-100 py-2" type="button" onClick={handleLogin}>Sign in</button>
+          <p className="mt-5 mb-3 text-body-secondary">© 2023–2028</p>
         </form>
       </main>
 
--- src/components/layouts/Full.layout.js
+++ src/components/layouts/Full.layout.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 
 const FullLayout = () => {
     return (
-      <div class="container-fluid p-0">
+      <div className="container-fluid p-0">
 
          <Navigation />
           <Outlet />
