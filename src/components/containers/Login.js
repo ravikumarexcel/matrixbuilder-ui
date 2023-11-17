e0c6475fd6a54b6be94f669e2e9847c0c3bb4822
ravikumargh <ravikumargh@gmail.com>
2023-11-17 12:57:39 +0530

    login page changes

--- src/components/containers/Login.js
+++ src/components/containers/Login.js
@@ -1,7 +1,8 @@
 // Login.js
 import React from 'react';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 import './Login.css';
+import logo from './../../logo.svg';
 
 const Login = () => {
   return (
@@ -14,60 +15,33 @@ const Login = () => {
     Button as link
   </Button> */}
 
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                  <h3 class="mb-3">Login</h3>
-                    <div class="bg-white shadow rounded">
-                        <div class="row">
-                            <div class=" pe-0">
-                                <div class="form-left h-100 py-5 px-5">
-                                    <form action="" class="row g-4">
-                                            <div class="col-12">
-                                                <label>Username<span class="text-danger">*</span></label>
-                                                <div class="input-group">
-                                                    <div class="input-group-text"><i class="bi bi-person-fill"></i></div>
-                                                    <input type="text" class="form-control" placeholder="Enter Username" />
-                                                </div>
-                                            </div>
 
-                                            <div class="col-12">
-                                                <label>Password<span class="text-danger">*</span></label>
-                                                <div class="input-group">
-                                                    <div class="input-group-text"><i class="bi bi-lock-fill"></i></div>
-                                                    <input type="text" class="form-control" placeholder="Enter Password" />
-                                                </div>
-                                            </div>
+      <main class="form-signin w-100 m-auto">
+        <form>
+          <img class="mb-4" src={logo} alt="" width="72" height="57" />
+          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-                                            <div class="col-sm-6">
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="inlineFormCheck" />
-                                                    <label class="form-check-label" for="inlineFormCheck">Remember me</label>
-                                                </div>
-                                            </div>
+          <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+            <label for="floatingPassword">Password</label>
+          </div>
 
-                                            <div class="col-sm-6">
-                                                <a href="#" class="float-end text-primary">Forgot Password?</a>
-                                            </div>
+          <div class="form-check text-start my-3">
+            <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+            <label class="form-check-label" for="flexCheckDefault">
+              Remember me
+            </label>
+          </div>
+          <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+          <p class="mt-5 mb-3 text-body-secondary">© 2023–2028</p>
+        </form>
+      </main>
 
-                                            <div class="col-12">
-                                                <button type="submit" class="btn btn-primary px-4 float-end mt-4">login</button>
-                                            </div>
-                                    </form>
-                                </div>
-                            </div>
-                            {/* <div class="col-md-5 ps-0 d-none d-md-block">
-                                <div class="form-right h-100 bg-primary text-white text-center pt-5">
-                                    <i class="bi bi-bootstrap"></i>
-                                    <h2 class="fs-1">Welcome Back!!!</h2>
-                                </div>
-                            </div> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        
 
- 
     </div>
   );
 };
