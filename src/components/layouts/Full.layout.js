2ccb269164e07f3ced0d5633e81edcafe2e4e45a
ravikumargh <ravikumargh@gmail.com>
2023-11-18 13:45:55 +0530

    token validation and token decode

--- src/components/layouts/Full.layout.js
+++ src/components/layouts/Full.layout.js
@@ -1,14 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navigation from './Navigation';
+import { useEffect } from "react";
 
 const FullLayout = () => {
-    return (
-      <div className="container-fluid p-0">
+  const navigate = useNavigate();
 
-         <Navigation />
-          <Outlet />
-      </div>
-    )
-  };
-  
-  export default FullLayout;
+  useEffect(() => {
+    // Check if a token exists in local storage
+    const storedToken = localStorage.getItem('token');
+    console.log(storedToken);
+
+    if (!storedToken) {
+      navigate('/auth/login');
+    }
+  })
+
+  return (
+    <div className="container-fluid p-0">
+
+      <Navigation />
+      <Outlet />
+    </div>
+  )
+};
+
+export default FullLayout;
--- src/components/layouts/Navigation.js
+++ src/components/layouts/Navigation.js
@@ -1,19 +1,37 @@
 // Example Navigation component with react-bootstrap
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 function Navigation() {
   const navigate = useNavigate();
+  // const [token, setToken] = useState(null);
+  const tokenObject = { 'Role': "", 'Issuer': "", 'Username': "", 'exp': "", 'iat': "" };
+  const [decodedToken, setDecodedToken] = useState(tokenObject);
+
   const handleLogout = (e) => {
     localStorage.removeItem('token');
-
     navigate('/auth/login');
   }
+
+  useEffect(() => {
+    // Check if a token exists in local storage
+    const storedToken = localStorage.getItem('token');
+    console.log(storedToken);
+
+    if (storedToken) {
+
+      const decoded = jwtDecode(storedToken);
+      setDecodedToken(decoded);
+      console.log(decoded);
+      // setDecodedToken(decoded);
+    }
+  }, [])
 
   return (
     <Navbar bg="light" expand="lg">
@@ -24,22 +42,23 @@ function Navigation() {
         <Nav className="me-auto">
           <LinkContainer to="/">
             <Nav.Link>
-            <FontAwesomeIcon icon={faHome} />
+              <FontAwesomeIcon icon={faHome} />
             </Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/auth/login">
-            <Nav.Link>Login</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/dashboard">
             <Nav.Link>Dashboard</Nav.Link>
           </LinkContainer>
         </Nav>
         <Nav className="mr-auto">
-           
-          <Button  variant="primary" onClick={handleLogout}>
-            Logout
-          </Button>
-           
+
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              {decodedToken.Username}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#" onClick={handleLogout}>Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
